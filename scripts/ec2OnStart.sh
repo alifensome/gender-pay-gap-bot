cc10bfa3335910f440c37eaec3e03f04cd740bbf
Alastair Fensome <alifensome@hotmail.co.uk>
2022-03-01 09:05:59 +0000

    Update ec2OnStart

--- scripts/ec2OnStart.sh
+++ scripts/ec2OnStart.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 echo "Runing boot up sequence!"
 touch /home/ec2-user/$(date +"%FT%H%M")
 cd /home/ec2-user/gender-pay-gap-bot
-git pull
-aws s3 cp s3://alifensome-general-bucket/gpga/.env .env
-npm run start:listener test >> /home/ec2-user/appLogs/logs
+#git pull
+#aws s3 cp s3://alifensome-general-bucket/gpga/.env .env
 
-#cd /home/ec2-user/gender-pay-gap-bot;npm run start:listener test >> /home/ec2-user/appLogs/logs
-#/home/ec2-user/gender-pay-gap-bot/scripts/ec2OnStart.sh
+/home/ec2-user/.nvm/versions/node/v17.6.0/bin/node dist/queueTweets/run.js >> /home/ec2-user/appLogs/logs
