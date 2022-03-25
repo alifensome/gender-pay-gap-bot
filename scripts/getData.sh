4ebac0a1ccbe8ea96d1e6c0359155abf7c006011
Alastair Fensome <alifensome@hotmail.co.uk>
2022-03-25 08:24:01 +0000

    Fixed vanerability in minst

--- scripts/getData.sh
+++ scripts/getData.sh
@@ -4,3 +4,4 @@ set -e
 wget https://gender-pay-gap.service.gov.uk/viewing/download-data/2021 \
 -O "./data/UK Gender Pay Gap Data - 2021 to 2022.csv"
  # TODO build to xlsn file.
+ # libreoffice --convert-to xlsx  ./data/UK\ Gender\ Pay\ Gap\ Data\ -\ 2021\ to\ 2022.csv 
