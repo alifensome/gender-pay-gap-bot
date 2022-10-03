84093a48bee5e3fbdbb6eae3408cc2a9f7d08c21
Ali Fensome <alifensome@hotmail.co.uk>
2022-10-03 21:51:37 +0100

    * main   plot-graph   refactor-company-type: Fix tests.

--- src/dataValidation/spotCheckData.test.ts
+++ src/dataValidation/spotCheckData.test.ts
@@ -1,83 +1,67 @@
-import DataImporter from "../importData"
-import { Repository } from "../importData/Repository"
-const importer = new DataImporter()
-const repo = new Repository(importer)
+import DataImporter from "../importData";
+import { Repository } from "../importData/Repository";
+import { CompanyDataMultiYearItem, CompanySize } from "../types";
+const importer = new DataImporter();
+const repo = new Repository(importer);
 
-const ryanAir = {
-    companyName: "Ryanair ltd",
-    companyNumber: null,
-    sicCodes: "51101",
-    gpg_2021_2022: 45.1,
-    gpg_2020_2021: 67.8,
-    gpg_2019_2020: null,
-    gpg_2018_2019: 62.2,
-    gpg_2017_2018: 67,
-    medianGpg_2021_2022: 42.8,
-    medianGpg_2020_2021: 68.6,
-    medianGpg_2019_2020: null,
-    medianGpg_2018_2019: 64.4,
-    medianGpg_2017_2018: 71.8,
-    size: "1000 to 4999",
-}
-const jamesFisherNuclearLimited = {
-    companyName: "JAMES FISHER NUCLEAR LIMITED",
-    companyNumber: "SC204768",
-    sicCodes: "62090",
-    gpg_2021_2022: 34.3,
-    gpg_2020_2021: 26.7,
-    gpg_2019_2020: null,
-    gpg_2018_2019: 32.4,
-    gpg_2017_2018: 33,
-    medianGpg_2021_2022: 29.7,
-    medianGpg_2020_2021: 21.6,
-    medianGpg_2019_2020: null,
-    medianGpg_2018_2019: 35.3,
-    medianGpg_2017_2018: 38.3,
-    size: "250 to 499",
-}
+const ryanAir: CompanyDataMultiYearItem = {
+  companyName: "Ryanair ltd",
+  companyNumber: null,
+  sicCodes: "51101",
+  data2021To2022: { meanGpg: 45.1, medianGpg: 42.8 },
+  data2020To2021: { meanGpg: 67.8, medianGpg: 68.6 },
+  data2019To2020: null,
+  data2018To2019: { meanGpg: 62.2, medianGpg: 64.4 },
+  data2017To2018: { meanGpg: 67, medianGpg: 71.8 },
+  size: CompanySize.From1000To4999,
+};
+const jamesFisherNuclearLimited: CompanyDataMultiYearItem = {
+  companyName: "JAMES FISHER NUCLEAR LIMITED",
+  companyNumber: "SC204768",
+  sicCodes: "62090",
+  data2021To2022: { meanGpg: 34.3, medianGpg: 29.7 },
+  data2020To2021: { meanGpg: 26.7, medianGpg: 21.6 },
+  data2019To2020: null,
+  data2018To2019: { meanGpg: 32.4, medianGpg: 35.3 },
+  data2017To2018: { meanGpg: 33, medianGpg: 38.3 },
+  size: CompanySize.From250To499,
+};
 
-const dorsetHealthcareNhsFoundationTrust = {
-    companyName: "Dorset Healthcare Nhs Foundation Trust",
-    companyNumber: null,
-    sicCodes: "1,86210",
-    gpg_2021_2022: 14.8,
-    gpg_2020_2021: 14.1,
-    gpg_2019_2020: null,
-    gpg_2018_2019: 16.4,
-    gpg_2017_2018: 19.1,
-    medianGpg_2021_2022: 8.7,
-    medianGpg_2020_2021: 8,
-    medianGpg_2019_2020: null,
-    medianGpg_2018_2019: 6.5,
-    medianGpg_2017_2018: 6.6,
-    size: "5000 to 19,999",
-}
+const dorsetHealthcareNhsFoundationTrust: CompanyDataMultiYearItem = {
+  companyName: "Dorset Healthcare Nhs Foundation Trust",
+  companyNumber: null,
+  sicCodes: "1,86210",
+  data2021To2022: { meanGpg: 14.8, medianGpg: 8.7 },
+  data2020To2021: { meanGpg: 14.1, medianGpg: 8 },
+  data2019To2020: null,
+  data2018To2019: { meanGpg: 16.4, medianGpg: 6.5 },
+  data2017To2018: { meanGpg: 19.1, medianGpg: 6.6 },
+  size: CompanySize.From5000To19999,
+};
 
-const doncasterMetropolitanBoroughCouncil = {
-    companyName: "Doncaster Metropolitan Borough Council",
-    companyNumber: null,
-    sicCodes: "1,84110",
-    gpg_2021_2022: 12.9,
-    gpg_2020_2021: 14.1,
-    gpg_2019_2020: 14.6,
-    gpg_2018_2019: 14.8,
-    gpg_2017_2018: 15.7,
-    medianGpg_2021_2022: 13.9,
-    medianGpg_2020_2021: 16,
-    medianGpg_2019_2020: 16.9,
-    medianGpg_2018_2019: 16.5,
-    medianGpg_2017_2018: 21.1,
-    size: "5000 to 19,999",
-}
+const doncasterMetropolitanBoroughCouncil: CompanyDataMultiYearItem = {
+  companyName: "Doncaster Metropolitan Borough Council",
+  companyNumber: null,
+  sicCodes: "1,84110",
+  data2021To2022: { meanGpg: 12.9, medianGpg: 13.9 },
+  data2020To2021: { meanGpg: 14.1, medianGpg: 16 },
+  data2019To2020: { meanGpg: 14.6, medianGpg: 16.9 },
+  data2018To2019: { meanGpg: 14.8, medianGpg: 16.5 },
+  data2017To2018: { meanGpg: 15.7, medianGpg: 21.1 },
+  size: CompanySize.From5000To19999,
+};
 
 describe("spot check data", () => {
-    it.each([
-        [ryanAir],
-        [jamesFisherNuclearLimited],
-        [dorsetHealthcareNhsFoundationTrust],
-        [doncasterMetropolitanBoroughCouncil]
-    ])("should get and check the company from the data", (company) => {
-        const companyFromData = repo.getCompany(company.companyName, company.companyNumber)
-        expect(companyFromData).toEqual(company)
-    })
-})
+  it.each([
+    [ryanAir],
+    [jamesFisherNuclearLimited],
+    [dorsetHealthcareNhsFoundationTrust],
+    [doncasterMetropolitanBoroughCouncil],
+  ])("should get and check the company from the data", (company) => {
+    const companyFromData = repo.getCompany(
+      company.companyName,
+      company.companyNumber
+    );
+    expect(companyFromData).toEqual(company);
+  });
+});
