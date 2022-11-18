db68b06486b019bb145f1ec4a926be877d499782
Alastair Fensome <alastair.fensome.ext@bbc.co.uk>
2022-11-18 17:47:25 +0000

    Added logging

--- src/queueTweets/run.ts
+++ src/queueTweets/run.ts
@@ -2,18 +2,24 @@ import dotEnv from "dotenv"
 import { SqsClient } from "../sqs/Client"
 import { Logger } from "tslog"
 import { TwitterClient } from "../twitter/Client"
-import { IncomingTweetListenerQueuer } from "./IncomingTweetListenerQueuer"
+import { IncomingTweetListenerQueuer, relevantWords } from "./IncomingTweetListenerQueuer"
 import DataImporter from "../importData"
 import { Repository } from "../importData/Repository"
 import { isDebugMode } from "../utils/debug"
 
+const logger = new Logger()
+logger.info(
+    JSON.stringify({
+        message: `starting listener. Listening for words: ${JSON.stringify(relevantWords)}`,
+        eventType: "startingListener",
+    })
+);
 dotEnv.config()
 
 const twitterClient = new TwitterClient()
 const sqsClient = new SqsClient()
 const dataImporter = new DataImporter()
 const repository = new Repository(dataImporter)
-const logger = new Logger()
 
 const handler = new IncomingTweetListenerQueuer(twitterClient, sqsClient, dataImporter, repository, logger)
 
