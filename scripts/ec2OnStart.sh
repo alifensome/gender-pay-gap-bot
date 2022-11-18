8bb6ff5063e42bc2664df8b2e4403f6188035a35
Alastair Fensome <alastair.fensome.ext@bbc.co.uk>
2022-11-18 19:11:26 +0000

    Remove errenous chown

--- scripts/ec2OnStart.sh
+++ scripts/ec2OnStart.sh
@@ -3,7 +3,7 @@
 echo "Runing boot up sequence!"
 touch /home/ec2-user/$(date +"%FT%H%M")
 cd /home/ec2-user/gender-pay-gap-bot
-sudo chown -R $USER ./
+#sudo chown -R $USER ./
 #git pull
 #aws s3 cp s3://alifensome-general-bucket/gpga/.env .env
 
