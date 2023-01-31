f295bceaac6de194cb02e3cca1e3588a2f1bd254
Alastair Fensome <alastair.fensome.ext@bbc.co.uk>
2023-01-31 22:27:52 +0000

    More changes

--- src/read/combineDataSets.ts
+++ src/read/combineDataSets.ts
@@ -20,32 +20,33 @@ const json2020 = dataImporter.gpg_2019_2020();
 const json2019 = dataImporter.gpg_2018_2019();
 const json2018 = dataImporter.gpg_2017_2018();
 
+console.log("Started reading data from 2022_2023");
+const data_2022_2023 = parseDataFromJson(json2023);
+console.log("Finished reading data from 2022_2023");
+
+console.log("Started reading data from 2021_2022");
+const data_2021_2022 = parseDataFromJson(json2022);
+console.log("Finished reading data from 2021_2022");
+
+console.log("Started reading data from 2020_2021");
+const data_2020_2021 = parseDataFromJson(json2021);
+console.log("Finished reading data from 2020_2021");
+
+console.log("Started reading data from 2019_2020");
+const data_2019_2020 = parseDataFromJson(json2020);
+console.log("Finished reading data from 2019_2020");
+
+console.log("Started reading data from 2018_2019");
+const data_2018_2019 = parseDataFromJson(json2019);
+console.log("Finished reading data from 2018_2019");
+
+console.log("Started reading data from 2017_2018");
+const data_2017_2018 = parseDataFromJson(json2018);
+console.log("Finished reading data from 2017_2018");
+
 getAllData().then(() => console.log("Finished!!!"));
 
 async function getAllData() {
-  console.log("Started reading data from 2022_2023");
-  const data_2022_2023 = await parseDataFromJson(json2023);
-  console.log("Finished reading data from 2022_2023");
-
-  console.log("Started reading data from 2021_2022");
-  const data_2021_2022 = await parseDataFromJson(json2022);
-  console.log("Finished reading data from 2021_2022");
-
-  console.log("Started reading data from 2020_2021");
-  const data_2020_2021 = await parseDataFromJson(json2021);
-  console.log("Finished reading data from 2020_2021");
-
-  console.log("Started reading data from 2019_2020");
-  const data_2019_2020 = await parseDataFromJson(json2020);
-  console.log("Finished reading data from 2019_2020");
-
-  console.log("Started reading data from 2018_2019");
-  const data_2018_2019 = await parseDataFromJson(json2019);
-  console.log("Finished reading data from 2018_2019");
-
-  console.log("Started reading data from 2017_2018");
-  const data_2017_2018 = await parseDataFromJson(json2018);
-  console.log("Finished reading data from 2017_2018");
 
   const numberOfItems2023 = data_2022_2023.length;
   const numberOfItems2022 = data_2021_2022.length;
@@ -55,6 +56,7 @@ async function getAllData() {
   const numberOfItems2018 = data_2017_2018.length;
 
   const totalData =
+    numberOfItems2023 +
     numberOfItems2022 +
     numberOfItems2021 +
     numberOfItems2020 +
@@ -63,6 +65,7 @@ async function getAllData() {
 
   console.log("Total Data:", totalData);
   console.log("Number of items:", {
+    numberOfItems2023,
     numberOfItems2022,
     numberOfItems2021,
     numberOfItems2020,
@@ -102,8 +105,16 @@ async function getAllData() {
       data_2017_2018
     );
 
+    const multiYearData: MultipleYearCompanyArg = {
+      item_2023: null,
+      item_2022: company,
+      item_2021: item_2021,
+      item_2020: item_2020,
+      item_2019: item_2019,
+      item_2018: item_2018
+    }
     combinedData.push(
-      toCompanyGpgDataItem(company, item_2021, item_2020, item_2019, item_2018)
+      toCompanyGpgDataItem(multiYearData)
     );
   }
 
@@ -192,7 +203,7 @@ async function getAllData() {
     );
 
     combinedData.push(
-      toCompanyGpgDataItem(null, null, null, company, item_2018)
+      (null, null, null, company, item_2018)
     );
   }
 
@@ -226,34 +237,32 @@ export interface Company {
   sicCodes: string;
 }
 
-// TODO make this more dynamic or update every year.
-function toCompanyGpgDataItem(
+interface MultipleYearCompanyArg {
   item_2023: Company | null,
   item_2022: Company | null,
   item_2021: Company | null,
   item_2020: Company | null,
   item_2019: Company | null,
   item_2018: Company | null
+}
+// TODO make this more dynamic or update every year.
+function toCompanyGpgDataItem(
+  multipleYearCompanyArg: MultipleYearCompanyArg,
 ): CompanyDataMultiYearItem {
   const latestCompanyObject = getLatestCompanyEntry(
-    item_2022,
-    item_2021,
-    item_2020,
-    item_2019,
-    item_2018
+    multipleYearCompanyArg
   );
   return {
     companyName: latestCompanyObject.companyName,
     companyNumber: latestCompanyObject.companyNumber,
     size: latestCompanyObject.size as CompanySize, // TODO parse this better!
     sicCodes: latestCompanyObject.sicCodes,
-
-    data2022To2023: toCompanyDataSingleYearItem(item_2023),
-    data2021To2022: toCompanyDataSingleYearItem(item_2022),
-    data2020To2021: toCompanyDataSingleYearItem(item_2021),
-    data2019To2020: toCompanyDataSingleYearItem(item_2020),
-    data2018To2019: toCompanyDataSingleYearItem(item_2019),
-    data2017To2018: toCompanyDataSingleYearItem(item_2018),
+    data2022To2023: toCompanyDataSingleYearItem(multipleYearCompanyArg.item_2023),
+    data2021To2022: toCompanyDataSingleYearItem(multipleYearCompanyArg.item_2022),
+    data2020To2021: toCompanyDataSingleYearItem(multipleYearCompanyArg.item_2021),
+    data2019To2020: toCompanyDataSingleYearItem(multipleYearCompanyArg.item_2020),
+    data2018To2019: toCompanyDataSingleYearItem(multipleYearCompanyArg.item_2019),
+    data2017To2018: toCompanyDataSingleYearItem(multipleYearCompanyArg.item_2018),
   };
 }
 
@@ -277,19 +286,10 @@ function toCompanyDataSingleYearItem(
 
 // TODO make this work more dynamically or remember to update it every year.
 function getLatestCompanyEntry(
-  item_2022: Company | null,
-  item_2021: Company | null,
-  item_2020: Company | null,
-  item_2019: Company | null,
-  item_2018: Company | null
+  multipleYearCompanyArg: MultipleYearCompanyArg
 ): Company {
-  const yearsOfCompany = [
-    item_2022,
-    item_2021,
-    item_2020,
-    item_2019,
-    item_2018,
-  ];
+
+  const yearsOfCompany = multipleYearsToList(multipleYearCompanyArg);
 
   for (let index = 0; index < yearsOfCompany.length; index++) {
     const element = yearsOfCompany[index];
@@ -300,6 +300,18 @@ function getLatestCompanyEntry(
   throw new Error(
     `there are no valid items for: ${JSON.stringify(yearsOfCompany)}`
   );
+}
+
+// TODO unit test this.
+function multipleYearsToList(multipleYearCompanyArg: MultipleYearCompanyArg) {
+  return [
+    multipleYearCompanyArg.item_2023,
+    multipleYearCompanyArg.item_2022,
+    multipleYearCompanyArg.item_2021,
+    multipleYearCompanyArg.item_2020,
+    multipleYearCompanyArg.item_2019,
+    multipleYearCompanyArg.item_2018,
+  ];
 }
 
 function isValidCompany(c: Company) {
@@ -313,4 +325,56 @@ function printPercentageComplete(current: number, totalData: number) {
 
 function percentageComplete(current: number, totalData: number) {
   return (current / totalData) * 100;
+}
+
+interface BasicCompanyInfo {
+  companyName: string,
+  companyNumber: string,
+}
+
+function combineYearsDataFrom(company: BasicCompanyInfo): CompanyDataMultiYearItem {
+  const item_2023 = findCompany(
+    company.companyName,
+    company.companyNumber,
+    data_2022_2023
+  );
+
+  const item_2022 = findCompany(
+    company.companyName,
+    company.companyNumber,
+    data_2021_2022
+  );
+
+
+  const item_2021 = findCompany(
+    company.companyName,
+    company.companyNumber,
+    data_2020_2021
+  );
+
+  const item_2020 = findCompany(
+    company.companyName,
+    company.companyNumber,
+    data_2019_2020
+  );
+  const item_2019 = findCompany(
+    company.companyName,
+    company.companyNumber,
+    data_2018_2019
+  );
+  const item_2018 = findCompany(
+    company.companyName,
+    company.companyNumber,
+    data_2017_2018
+  );
+  const multiYearData: MultipleYearCompanyArg = {
+    item_2023,
+    item_2022,
+    item_2021,
+    item_2020,
+    item_2019,
+    item_2018,
+  }
+
+  return toCompanyGpgDataItem(multiYearData)
 }
