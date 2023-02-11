5784b50ffd15b5b582a3ada30b3e899142c3e462
Ali Fensome <alifensome@hotmail.co.uk>
2023-02-11 18:44:20 +0000

    add-2022   depoy-ec2-improvements * main   plot-graph   refactor-company-type: Clear up deploy listener script

--- scripts/deployListener.sh
+++ scripts/deployListener.sh
@@ -2,11 +2,12 @@
 
 set -e
 
+echo "Building project..."
 npm run build
 npm run test
 
+echo "Uploading deployment..."
 aws s3 sync dist s3://alifensome-general-bucket/gpga/dist
-# aws s3 sync node_modules s3://alifensome-general-bucket/gpga/node_modules  // maybe???
 aws s3 sync data s3://alifensome-general-bucket/gpga/data
 aws s3 sync scripts s3://alifensome-general-bucket/gpga/scripts
 aws s3 cp package.json s3://alifensome-general-bucket/gpga/package.json
@@ -16,3 +17,7 @@ aws s3 cp package-lock.json s3://alifensome-general-bucket/gpga/package-lock.jso
 echo "Restarting listener ID: $EC2_ID"
 source ./private/var.sh 
 aws ec2 reboot-instances --instance-ids $EC2_ID
+echo "Finished deloyment."
+
+echo "listening for logs..."
+aws logs tail /aws/ec2/gpga-listener --follow
