1579edd2ebaf8b6f1ed2d52c213964771893063c
Ali Fensome <alifensome@hotmail.co.uk>
2023-02-16 19:36:31 +0000

    add-2022   depoy-ec2-improvements * main   plot-graph   refactor-company-type: copy writing tests

--- src/copyWriter/copyWriter.test.ts
+++ src/copyWriter/copyWriter.test.ts
@@ -9,4 +9,16 @@ describe("copyWriter", () => {
     const c = new CopyWriter();
     c.getDifferenceCopy(mockCompanyDataItem);
   });
+  test.todo(
+    "should get the copy for the (mean/median) gpg and show the difference between years"
+  );
+  test.todo(
+    "should get the copy for the (mean/median) gpg and show the difference between years even with one years data missing"
+  );
+  test.todo(
+    "should get the copy for just the (mean/median) gpg when theres not enough data points"
+  );
+  test.todo(
+    "should get the copy for just the (mean/median) gpg when theres not enough data points returning the latest data regardless of year"
+  );
 });
