1e3c7f0596c3f2b866bc5ea655ea935342bbf6c8
Alastair Fensome <alastair.fensome.ext@bbc.co.uk>
2023-02-22 10:03:24 +0000

    Fix data script

--- src/dataFix/fixTwitterIds.ts
+++ src/dataFix/fixTwitterIds.ts
@@ -3,10 +3,17 @@ import DataImporter from '../importData'
 import { TwitterClient } from 'twitter-api-client';
 import { writeJsonFile } from "../utils/write.js";
 import { getEnvVar } from "../utils/getEnvVar";
+import { promptUser } from "../utils/promptUser";
 const dataImporter = new DataImporter()
 
 dotEnv.config()
-const companyDataProd = dataImporter.twitterUserDataProd()
+
+const path = process.argv0
+console.log(`Fixing data fro path ${path}. Continue?`)
+
+
+// readfile 
+// const companyDataProd = dataImporter.twitterUserDataProd()
 
 const twitterClient = new TwitterClient({
     apiKey: getEnvVar('TWITTER_API_KEY'),
@@ -16,28 +23,41 @@ const twitterClient = new TwitterClient({
 });
 
 async function run() {
+    const shouldContinue = await promptUser('Continue?');
+
+    if (shouldContinue !== 'y') {
+        process.exit(0)
+    }
+    const dataToFix = dataImporter.readFile(path)
 
     const maxValue = 2147483647
     let numberOfErrors = 0
     const newCompanyData: any[] = []
-    for (let index = 0; index < companyDataProd.length; index++) {
+    for (let index = 0; index < dataToFix.length; index++) {
         if (index % 100 === 0) {
-            console.log(`${(index / companyDataProd.length) * 100}%`)
+            console.log(`${(index / dataToFix.length) * 100}%`)
         }
-        const row = companyDataProd[index];
+        const row = dataToFix[index];
+        if (row.twitter_id_str) {
+            newCompanyData.push(row)
+            continue;
+        }
+        if (!row.twitter_id) {
+            throw new Error(`no twitterID found ${JSON.stringify(row)}`)
+        }
         let userIdStr = `${row.twitter_id}`
-        if (row.twitter_id > maxValue) {
+        if (row.twitter_id >= maxValue) {
             numberOfErrors++
             const user = await twitterClient.accountsAndUsers.usersLookup({ screen_name: row.twitter_screen_name })
-            console.log("Found ", user[0].screen_name)
+            console.log(`Found ${row.companyName}, ${user[0].screen_name}, ID: ${user[0].id_str}.`)
             userIdStr = user[0].id_str
         }
         row.twitter_id_str = userIdStr
         newCompanyData.push(row)
     }
 
-    writeJsonFile("./dataFix/twitterIds.json", newCompanyData)
-    console.log(numberOfErrors)
+    await writeJsonFile(path, newCompanyData)
+    console.log(`Number of items: ${newCompanyData}. Number of errors:${numberOfErrors}.`)
 }
 
 run()
