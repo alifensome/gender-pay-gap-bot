3abb9a216ca50bc183c7588abe86ef265a743d0a
Ali Fensome <alifensome@hotmail.co.uk>
2023-02-24 18:24:25 +0000

    add-2022   depoy-ec2-improvements * main   plot-graph   refactor-company-type   tweet-difference: Try catch run

--- scripts/ec2OnStart.sh
+++ scripts/ec2OnStart.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -ex
 
 echo "Runing boot up sequence!"
+echo "Starting..." >> /home/ec2-user/appLogs/logs
 touch /home/ec2-user/$(date +"%FT%H%M")
 cd /home/ec2-user/gender-pay-gap-bot
+
+/usr/local/bin/aws configure list
+
 #sudo chown -R $USER ./
 # sudo chown -R USERID.USERID *
 #git pull
@@ -10,9 +14,9 @@ cd /home/ec2-user/gender-pay-gap-bot
 
 # aws s3 sync s3://alifensome-general-bucket/gpga/ .
 
-/usr/local/bin/aws s3 sync s3://alifensome-general-bucket/gpga/data data
-#/usr/local/bin/aws s3 sync s3://alifensome-general-bucket/gpga/scripts scripts
-/usr/local/bin/aws s3 sync s3://alifensome-general-bucket/gpga/dist dist
+/usr/local/bin/aws s3 sync s3://alifensome-general-bucket/gpga/data data >> /home/ec2-user/appLogs/logs
+#/usr/local/bin/aws s3 sync s3://alifensome-general-bucket/gpga/scripts scripts >> /home/ec2-user/appLogs/logs
+/usr/local/bin/aws s3 sync s3://alifensome-general-bucket/gpga/dist dist >> /home/ec2-user/appLogs/logs
 
 
 /home/ec2-user/.nvm/versions/node/v17.6.0/bin/node dist/queueTweets/run.js >> /home/ec2-user/appLogs/logs
--- src/queueTweets/run.ts
+++ src/queueTweets/run.ts
@@ -9,37 +9,43 @@ import { Repository } from "../importData/Repository";
 import { isDebugMode } from "../utils/debug";
 import { getEnvVar } from "../utils/getEnvVar";
 
-const logger = new Logger();
+try {
+  const logger = new Logger();
 
-logger.info("Started listening at time: " + new Date().toISOString());
+  logger.info("Started listening at time: " + new Date().toISOString());
 
-logger.info(
-  JSON.stringify({
-    message: `starting listener. Listening for words: ${JSON.stringify(
-      relevantWords
-    )}`,
-    eventType: "startingListener",
-  })
-);
-dotEnv.config();
+  logger.info(
+    JSON.stringify({
+      message: `starting listener. Listening for words: ${JSON.stringify(
+        relevantWords
+      )}`,
+      eventType: "startingListener",
+    })
+  );
+  dotEnv.config();
 
-const twitterClient = new TwitterClient();
-const sqsClient = new SqsClient();
-const sqsClientTweetAtGpga = new SqsClient(
-  getEnvVar("TWEET_AT_GPGA_SQS_QUEUE_URL")
-);
-const dataImporter = new DataImporter();
-const repository = new Repository(dataImporter);
+  const twitterClient = new TwitterClient();
+  const sqsClient = new SqsClient();
+  const sqsClientTweetAtGpga = new SqsClient(
+    getEnvVar("TWEET_AT_GPGA_SQS_QUEUE_URL")
+  );
+  const dataImporter = new DataImporter();
+  const repository = new Repository(dataImporter);
 
-const handler = new IncomingTweetListenerQueuer(
-  twitterClient,
-  sqsClient,
-  sqsClientTweetAtGpga,
-  dataImporter,
-  repository,
-  logger
-);
+  const handler = new IncomingTweetListenerQueuer(
+    twitterClient,
+    sqsClient,
+    sqsClientTweetAtGpga,
+    dataImporter,
+    repository,
+    logger
+  );
 
-const isTest = isDebugMode();
+  const isTest = isDebugMode();
 
-handler.listen(isTest);
+  handler.listen(isTest);
+} catch (err) {
+  console.log("threw while starting!");
+  console.error(err);
+  process.exit(1);
+}
