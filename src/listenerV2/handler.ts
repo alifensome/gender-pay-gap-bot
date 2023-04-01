ca8e6b2030774f1121aeaac12af3a7c5d5533e44
Ali Fensome <alifensome@hotmail.co.uk>
2023-04-01 16:58:08 +0100

    add-2022   depoy-ec2-improvements * main   plot-graph   refactor-company-type   tweet-difference: Lambda for v2 listener

---
+++ src/listenerV2/handler.ts
@@ -0,0 +1,30 @@
+import { ListenerV2 } from "./ListenerV2";
+
+import DataImporter from "../importData";
+import { Repository } from "../importData/Repository";
+import { TwitterClient } from "../twitter/Client";
+import { getEnvVar } from "../utils/getEnvVar";
+import { SqsClient } from "../sqs/Client";
+import { LambdaLogger } from "../lambdaLogger";
+import { SearchQueryFormer } from "./searchQueryFormer";
+
+const dataImporter = new DataImporter();
+const twitterClient = new TwitterClient();
+const repo = new Repository(dataImporter);
+const sqsClientTweetAtGpga = new SqsClient(getEnvVar("SQS_QUEUE_URL"));
+
+/**
+ * TODO
+ * 1. pagination?
+ * 2. Check the recent time/start time thing.
+ */
+export async function handle(): Promise<void> {
+  const listenerV2 = new ListenerV2(
+    twitterClient,
+    sqsClientTweetAtGpga,
+    repo,
+    new LambdaLogger("listenerV2"),
+    new SearchQueryFormer()
+  );
+  await listenerV2.run();
+}
