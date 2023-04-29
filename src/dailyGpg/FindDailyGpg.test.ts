6f53313fbea18fc9c8c6648704832f574a87af66
Ali Fensome <alifensome@hotmail.co.uk>
2023-04-29 18:49:59 +0100

    test

--- src/dailyGpg/FindDailyGpg.test.ts
+++ src/dailyGpg/FindDailyGpg.test.ts
@@ -1,49 +1,53 @@
-import { daysSinceBeginningOfYear, findDailyGpg, findPercentageGpgRange } from "./FindDailyGpg"
-const d = new Date(2022, 2, 13, 1, 2, 3)
+import {
+  daysSinceBeginningOfYear,
+  findDailyGpg,
+  findPercentageGpgRange,
+} from "./FindDailyGpg";
+const d = new Date(2022, 2, 13, 1, 2, 3);
 describe("findDailyGpg", () => {
-    it("should get the companies with a GPG for that day", () => {
-        findDailyGpg(d)
-    })
-})
+  it("should get the companies with a GPG for that day", () => {
+    findDailyGpg(d);
+  });
+});
 
 describe("findPercentageGpgRange", () => {
-    it("should  find a range of percentages for the last day of the year", () => {
-        const middleOfYear = new Date(2022, 11, 31)
-        const result = findPercentageGpgRange(middleOfYear)
-        expect(result).toEqual({
-            startPercentage: 0,
-            endPercentage: 100 / 365,
-        })
-    })
-    it("should  find a range of percentages for the first day of the year", () => {
-        const middleOfYear = new Date(2022, 0, 1)
-        const result = findPercentageGpgRange(middleOfYear)
-        expect(result).toEqual({
-            startPercentage: 100 - 100 / 365,
-            endPercentage: 100,
-        })
-    })
-    it("should  find a range of percentages for middle of year", () => {
-        const middleOfYear = new Date(2022, 6, 2)
-        const result = findPercentageGpgRange(middleOfYear)
-        expect(result).toEqual({
-            "endPercentage": 50.410958904109584,
-            "startPercentage": 50.13698630136986,
-        })
-    })
-})
+  it("should  find a range of percentages for the last day of the year", () => {
+    const middleOfYear = new Date(2022, 11, 31);
+    const result = findPercentageGpgRange(middleOfYear);
+    expect(result).toEqual({
+      startPercentage: 0,
+      endPercentage: 100 / 365,
+    });
+  });
+  it("should  find a range of percentages for the first day of the year", () => {
+    const middleOfYear = new Date(2022, 0, 1);
+    const result = findPercentageGpgRange(middleOfYear);
+    expect(result).toEqual({
+      startPercentage: 100 - 100 / 365,
+      endPercentage: 100,
+    });
+  });
+  it("should find a range of percentages for middle of year", () => {
+    const middleOfYear = new Date(2022, 6, 2, 0, 0, 0);
+    const result = findPercentageGpgRange(middleOfYear);
+    expect(result).toEqual({
+      endPercentage: 50.410958904109584,
+      startPercentage: 50.13698630136986,
+    });
+  });
+});
 
 describe("daysSinceBeginningOfYear", () => {
-    it("should find days since beginning of year", () => {
-        const days = daysSinceBeginningOfYear(d)
-        expect(days).toBe(72)
-    })
-    it("should find days since beginning of year", () => {
-        const days = daysSinceBeginningOfYear(new Date(2022, 0, 1))
-        expect(days).toBe(1)
-    })
-    it("should find days since beginning of year", () => {
-        const days = daysSinceBeginningOfYear(new Date(2022, 11, 31))
-        expect(days).toBe(365)
-    })
-})
+  it("should find days since beginning of year", () => {
+    const days = daysSinceBeginningOfYear(d);
+    expect(days).toBe(72);
+  });
+  it("should find days since beginning of year", () => {
+    const days = daysSinceBeginningOfYear(new Date(2022, 0, 1));
+    expect(days).toBe(1);
+  });
+  it("should find days since beginning of year", () => {
+    const days = daysSinceBeginningOfYear(new Date(2022, 11, 31));
+    expect(days).toBe(365);
+  });
+});
